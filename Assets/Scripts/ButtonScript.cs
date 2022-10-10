407136836fbe53ac20299e3f12439a0ccdac4022
Mattéo Benaissa <92030744+MatteoBenaissaGit@users.noreply.github.com>
2022-10-10 12:03:59 +0200

    menu correct

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,9 +12,16 @@ public class ButtonScript : MonoBehaviour
     {
         sound = Sound.Instance;
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+            PlayButton();
+    }
+
     public void PlayButton()
     {
-        sound.MenuClick(true);
+        //sound.MenuClick(true);
         SceneManager.LoadScene("GameScene");
     }
 }
